269c269ebca7165e59f0ed44c3a7703d4857e765
subhashis nath <subhashis.nath@emc.com>
2019-07-05 09:48:46 +0530

    API gateway service using Netflix zuul and spring boot

---
+++ src/main/java/org/subhashis/mycroservices/mynetflixzuulapigatewayserver/MyNetflixZuulApiGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package org.subhashis.mycroservices.mynetflixzuulapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNetflixZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNetflixZuulApiGatewayServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/subhashis/mycroservices/mynetflixzuulapigatewayserver/MyNetflixZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.subhashis.mycroservices.mynetflixzuulapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNetflixZuulApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
